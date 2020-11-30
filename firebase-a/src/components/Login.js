e07280bcc8e344bf56a7723ada043aecb634688d
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-11-30 01:44:03 -0500

    Setting up react router and login page

--- firebase-a/src/components/Login.js
+++ firebase-a/src/components/Login.js
@@ -2,11 +2,10 @@ import React, { useRef, useState } from 'react'
 import {Form, Button, Card, Alert} from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 
-export default function Signup() {
+export default function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
     const { signup } = useAuth()
     const[error, setError] =useState('')
     const[loading, setLoading] =useState(false)
@@ -14,10 +13,6 @@ export default function Signup() {
 
     async function handleSubmit(e){
       e.preventDefault()
-
-      if(passwordRef.current.value !==passwordConfirmRef.current.value){
-        return setError('Passwords do not match')
-      }
 
       try {
         setError('')
@@ -36,21 +31,20 @@ export default function Signup() {
             <Card.Body>
                 <h2 className = 'text-center mb-4'>Signup</h2>
                 {error && <Alert variant = "danger"> {error} </Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group id='email'>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type='email' ref={emailRef} required />
-                     </Form.Group>
+            <Form onSubmit={handleSubmit}>
+
+                 <Form.Group id='email'>
+                     <Form.Label>Email</Form.Label>
+                        <Form.Control type='email' ref={emailRef} required />
+                 </Form.Group>
+
                      <Form.Group id='password'>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' ref={passwordRef} required />
+                         <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' ref={passwordRef} required />
                      </Form.Group>
-                     <Form.Group id='password-confirm'>
-                    <Form.Label>Password Confirmation</Form.Label>
-                    <Form.Control type='password' ref={passwordConfirmRef} required />
-                     </Form.Group>
+                  
                      <Button disabled={loading} className='w-100' type='submit'> Sign up </Button>
-                     </Form>
+             </Form>
             </Card.Body>
             </Card>  
           <div className='w-100 text-center mt-2'>
--- firebase-a/src/contexts/AuthContext.js
+++ firebase-a/src/contexts/AuthContext.js
@@ -8,7 +8,8 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-	const [ currentUser, setCurrentUser ] = useState();
+    const [ currentUser, setCurrentUser ] = useState();
+    const [loading, setLoading] = useState(true)
 
 	function signup(email, password) {
 		return auth.createUserWithPassword(email, password);
@@ -16,7 +17,9 @@ export function AuthProvider({ children }) {
     
     useEffect(() => {
     const unsubscribe =  auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
+        setCurrentUser(user)
+        setLoading(false)
+            
         })
 
         return unsubscribe
@@ -28,5 +31,9 @@ export function AuthProvider({ children }) {
         signup
 	};
 
-	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	return (
+        <AuthContext.Provider value={value}>
+        {!loading && children}
+        </AuthContext.Provider>
+    )
 }
